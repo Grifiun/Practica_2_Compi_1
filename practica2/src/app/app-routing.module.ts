f9d0e1dc6b834755d5e202bbcbdc528c1caebbf6
Grifiun <denii.grifon@gmail.com>
2021-04-14 21:38:34 -0600

    se agrego la funcionalidad del contador de lineas

--- practica2/src/app/app-routing.module.ts
+++ practica2/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
  {
   path: '',
   component: SkeletonComponent,   
-  children: [
+  children: [ 
     {
       path: '',
       loadChildren: () =>
--- practica2/src/app/modules/entrada/entrada-texto/entrada-texto.component.ts
+++ practica2/src/app/modules/entrada/entrada-texto/entrada-texto.component.ts
@@ -5,10 +5,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entrada-texto.component.css']
 })
 export class EntradaTextoComponent implements OnInit {
-  
+  contador: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  cambioTexto(texto: string){
+    this.contador = '';
+    //alert(texto);
+    //contamos las lineas
+    var lines = texto.split(/\r|\r\n|\n/);
+    var count = lines.length;
+
+    //agregamos el valor
+    for(var i = 1; i <= count; i++){
+      this.contador += ''+i+'\n';
+    }
+    //alert(this.contador);
+  }
+
+
 
 }
